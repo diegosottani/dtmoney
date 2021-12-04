74f55e0677c76b65a1d2e19f2b5fe0661dbf1838
Diego <diegosottani@gmail.com>
2021-12-03 21:27:12 -0300

    Terminando layout do dashboard

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import { createServer } from 'miragejs';
 import { App } from './App';
+
+// createServer({
+//   routes(){
+//     this.namespace = 'api';
+
+//     this.get('')
+//   }
+// })
 
 ReactDOM.render(
   <React.StrictMode>
