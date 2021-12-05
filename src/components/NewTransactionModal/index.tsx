6d20a7fde526b63e3791c40f63dbd21d5dbaca01
Diego <diegosottani@gmail.com>
2021-12-05 14:46:06 -0300

    Estilização do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
-import Container from './styles'
-//import { Container, Content } from './styles'
 import Modal from "react-modal";
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import closeImg from '../../assets/close.svg'
+import { Container, TransactionTypeContainer } from './styles'
 interface NewTransactionModalProps{
     isOpen: boolean
     onRequestClose: () => void
@@ -14,12 +16,28 @@ export function NewTransactionModal(props: NewTransactionModalProps) {
             overlayClassName="react-modal-overlay"
             className="react-modal-content"
         >
+            <button type="button" onClick={props.onRequestClose} className="react-modal-close">
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
             <Container>
                 <h2>Cadastrar transação</h2>
                 <input placeholder="Título" />
                 <input type="number" placeholder="Valor" />
                 <input placeholder="Categoria" />
-                <button type="submit"></button>
+
+                <TransactionTypeContainer>
+                    <button type="button">
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+
+                    <button type="button">
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Entrada</span>
+                    </button>
+                </TransactionTypeContainer>
+
+                <button type="submit">Cadastrar</button>
             </Container>
         </Modal>
     )
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -54,7 +54,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     .react-modal-overlay{
-        background: rgba(0, 0, 0, 0,5);
+        background: rgba(0, 0, 0, 0.5);
 
         position: fixed;
         top: 0;
@@ -70,10 +70,23 @@ export const GlobalStyle = createGlobalStyle`
     .react-modal-content{
         width: 100%;
         max-width: 576px;
-        background: var(---background);
+        background: var(--background);
         padding: 3rem;
         position: relative;
         border-radius: 0.24rem;
+    }
+
+    .react-modal-close{
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+        transition: filter 0.2s;
+
+        &:hover{
+            filter: brightness(0.8);
+        }
     }
 
 `
